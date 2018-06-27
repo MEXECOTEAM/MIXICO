124e0fd7cbc9d6c6f389d6d0d2dadb5ba8e72362
MEXECOTEAM <40605471+MEXECOTEAM@users.noreply.github.com>
2018-06-27 19:32:08 +0300

    Update functions.lua

--- inc/functions.lua
+++ inc/functions.lua
@@ -1,6 +1,6 @@
 --[[
 << تم برمجه وكتابه السورس من قبل مطورين >>
-<<         @BLCON  \  @TH3BOSS          >>
+<<         @VI_P2  \  @MEXICO          >>
 ]]
 function dl_cb(dol, info) end 
 function getInputFile(file)
